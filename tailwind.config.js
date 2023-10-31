93fc961542affe2b98830ccef954af6dfba0bb2f
wahyu firmansyah <whyfirmansyah69@gmail.com>
2023-10-31 15:46:59 +0700

    menambahkan data

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '32px',
+    },
+    extend: {
+      colors: {
+        primary: '#032ff2',
+        secondary: '#f1f8ff',  
+        dark: '#0f172a',
+        orange: '#f58725',
+        dongker: '#202c6c',
+        orangemuda : '#fef9f3',
+      },
+      screens: {
+        '2xl': '1720px',
+      }
+    },
+  },
+  plugins: [],
+}
